054152019cb9d06c353a62880df05e2476670432
Dzmitry Churadze <Dzmitry.Churadze@refinitiv.com>
2023-03-19 18:56:56 +0100

    feat(task-5): s3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,15 +5,15 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://yhev2m8qhh.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://yhev2m8qhh.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
-    bff: false,
+    import: true,
+    bff: true,
     cart: false,
   },
 };
